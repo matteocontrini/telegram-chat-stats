c42fff155dbcf6f667f8505599692eee67629529
Matteo Contrini <matteo@contrini.it>
2022-10-16 00:00:12 +0200

    Initial version

---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+import telebot
+from pyrogram import Client
+
+tz = ZoneInfo('Europe/Rome')
+
+API_ID = os.environ['API_ID']
+API_HASH = os.environ['API_HASH']
+BOT_TOKEN = os.environ['BOT_TOKEN']
+CHAT_ID = os.environ['CHAT_ID']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+SESSION_NAME = os.environ['SESSION_NAME']
+
+tg = Client(SESSION_NAME, API_ID, API_HASH)
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode='HTML')
+
+
+async def get_yesterday_counts() -> (dict, dict):
+    max_date = datetime.now(tz).replace(hour=0, minute=0, second=0, microsecond=0)
+    min_date = max_date - timedelta(days=1)
+
+    total = {
+        'count': 0,
+        'text': 0,
+        'media': 0,
+        'chars': 0,
+    }
+    counts = {}
+    async for message in tg.get_chat_history(CHAT_ID):
+        date = message.date.astimezone(tz)
+        if date > max_date:
+            continue
+        if date < min_date:
+            break
+        name = message.from_user.first_name
+        if name not in counts:
+            counts[name] = {
+                'count': 0,
+                'text': 0,
+                'media': 0,
+                'chars': 0,
+            }
+        counts[name]['count'] += 1
+        total['count'] += 1
+        if message.media:
+            counts[name]['media'] += 1
+            total['media'] += 1
+            counts[name]['chars'] += len(message.caption or '')
+            total['chars'] += len(message.caption or '')
+        else:
+            counts[name]['text'] += 1
+            counts[name]['chars'] += len(message.text)
+            total['text'] += 1
+            total['chars'] += len(message.text)
+    return total, counts
+
+
+async def get_cum() -> int:
+    return await tg.search_messages_count(CHAT_ID)
+
+
+async def main():
+    async with tg:
+        totals, counts = await get_yesterday_counts()
+
+        yesterday = datetime.now(tz).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+        pretty_date = yesterday.strftime('%Y-%m-%d')
+
+        text = f'🗓 <b>{pretty_date}</b>\n\n'
+
+        text += f'<b>Total</b>:\n'
+
+        text += '<pre>'
+        for key, value in totals.items():
+            text += f' {key}: <b>{value}</b>\n'
+        text += '</pre>'
+
+        for name, counts in counts.items():
+            text += f'\n<b>{name}</b>:\n'
+            text += '<pre>'
+            for key, value in counts.items():
+                text += f' {key}: <b>{value}</b>\n'
+            text += '</pre>'
+
+        cum = await get_cum()
+        text += f'\n📈 <b>Cum:</b> <code>{cum}</code>'
+
+        print(text)
+
+        bot.send_message(CHANNEL_ID, text)
+
+
+if __name__ == '__main__':
+    tg.run(main())
